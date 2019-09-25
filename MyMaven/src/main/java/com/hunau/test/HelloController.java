da224fb27ab7623c9cccb43da0998a03e6411438
CaoChengMin <1921997690@qq.com>
2019-09-25 15:00:41 +0800

    Maven程序示例

---
+++ MyMaven/src/main/java/com/hunau/test/HelloController.java
@@ -0,0 +1,14 @@
+package com.hunau.test;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sang on 2018/7/4.
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "曹诚敏，学号为201841882223";
+    }
+}
